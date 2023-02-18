8b075aa1f18da21c1b93d1ab52a717538a03ebd9
EomYerim <djadpfa1002@naver.com>
2023-02-18 23:31:01 +0900

    Lv.1 문제 풀이

---
+++ PGS/Lv.1/명예의 전당 (1).cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+	vector<int> answer;
+
+	priority_queue<int> pq;
+	for (int i = 0; i < score.size(); i++) {
+		if (i < k) {
+			pq.push(-score[i]);
+		}
+		else {
+			if (-pq.top() < score[i]) {
+				pq.pop();
+				pq.push(-score[i]);
+			}
+		}
+		answer.push_back(-pq.top());
+	}
+	return answer;
+}
