a4174e8e0a75a98ea9ffbde3c2911a184039c9fa
EomYerim <djadpfa1002@naver.com>
2023-02-22 23:59:10 +0900

    Lv.0 문제 풀이 2

--- PGS/Lv.0/영어가 싫어요.cpp
+++ PGS/Lv.0/영어가 싫어요.cpp
@@ -1,62 +1,25 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 long long solution(string numbers) {
 	long long answer = 0;
-	string sNumber;
 
-	int i = 0;
-	while (i < numbers.size()) {
-		if (numbers[i] == 'z') {
-			sNumber += '0';
-			i += 4;
-		}
-		else if (numbers[i] == 'o') {
-			sNumber += '1';
-			i += 3;
-		}
-		else if (numbers[i] == 't') {
-			if (numbers[i + 1] == 'w') {
-				sNumber += '2';
-				i += 3;
-			}
-			else {
-				sNumber += '3';
-				i += 5;
-			}
-		}
-		else if (numbers[i] == 'f') {
-			if (numbers[i + 1] == 'o') {
-				sNumber += '4';
-			}
-			else {
-				sNumber += '5';
-			}
-			i += 4;
-		}
-		else if (numbers[i] == 's') {
-			if (numbers[i + 1] == 'i') {
-				sNumber += '6';
-				i += 3;
-			}
-			else {
-				sNumber += '7';
-				i += 5;
-			}
-		}
-		else if (numbers[i] == 'e') {
-			sNumber += '8';
-			i += 5;
-		}
-		else if (numbers[i] == 'n') {
-			sNumber += '9';
-			i += 4;
+	vector<string> v = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+	string num = "";
+	string tmp = "";
+	for (auto i : numbers) {
+		tmp += i;
+		if (find(v.begin(), v.end(), tmp) != v.end()) {
+			int idx = find(v.begin(), v.end(), tmp) - v.begin();
+			num += to_string(idx);
+			tmp = "";
 		}
 	}
 
-	answer = stoll(sNumber);
+	answer = stoll(num);
 
 	return answer;
 }
