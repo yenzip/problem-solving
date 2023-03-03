a88bc4b0a0eb0867711831b404aa24fd12ffff56
EomYerim <djadpfa1002@naver.com>
2023-03-03 23:39:10 +0900

    Lv.0 문제 풀이 2

--- PGS/Lv.0/안전지대.cpp
+++ PGS/Lv.0/안전지대.cpp
@@ -1,31 +1,27 @@
 #include <string>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
-int N, M;
-bool visited[100][100];
-int dRow[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dCol[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-int danger = 0;
+int N;
+vector<vector<int>> map;
+int dX[8] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int dY[8] = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-int isOut(int row, int col) {
-	return (row < 0 || row >= N || col < 0 || col >= M);
+int isOut(int x, int y) {
+	return (x < 0 || x >= N || y < 0 || y >= N);
 }
 
-void check(vector<vector<int>> board, int row, int col) {
-	visited[row][col] = true;
-	danger++;
-
+void unsafe(int x, int y) {
 	for (int i = 0; i < 8; i++) {
-		int nextRow = row + dRow[i];
-		int nextCol = col + dCol[i];
+		int nX = x + dX[i];
+		int nY = y + dY[i];
 
-		if (!isOut(nextRow, nextCol) && board[nextRow][nextCol] == 0 && !visited[nextRow][nextCol]) {
-			visited[nextRow][nextCol] = true;
-			danger++;
+		if (isOut(nX, nY) || map[nX][nY] == 1) {
+			continue;
 		}
+
+		map[nX][nY] = 1;
 	}
 }
 
@@ -33,19 +29,23 @@ int solution(vector<vector<int>> board) {
 	int answer = 0;
 
 	N = board.size();
-	M = board[0].size();
-
-	memset(visited, false, sizeof(visited));
+	map.insert(map.begin(), board.begin(), board.end());
 
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (board[i][j] == 1 && !visited[i][j]) {
-				check(board, i, j);
+		for (int j = 0; j < N; j++) {
+			if (board[i][j] == 1) {
+				unsafe(i, j);
 			}
 		}
 	}
 
-	answer = N * M - danger;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == 0) {
+				answer++;
+			}
+		}
+	}
 
 	return answer;
 }
