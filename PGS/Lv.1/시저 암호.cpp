3f105059a6f95a41a608353472eb4a859e4d91c7
EomYerim <djadpfa1002@naver.com>
2023-03-29 17:32:32 +0900

    Lv.1 문제 풀이 2

--- PGS/Lv.1/시저 암호.cpp
+++ PGS/Lv.1/시저 암호.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 string solution(string s, int n) {
 	string answer = "";
-	for (auto i : s) {
-		if (isupper(i)) {
-			answer += 'A' + ((i - 'A') + n) % 26;
+
+	for (char c : s) {
+		if (isupper(c)) {
+			answer += 'A' + (c - 'A' + n) % 26;
 		}
-		else if (islower(i)) {
-			answer += 'a' + ((i - 'a') + n) % 26;
+		else if (islower(c)) {
+			answer += 'a' + (c - 'a' + n) % 26;
 		}
 		else {
-			answer += i;
+			answer += c;
 		}
 	}
+	
 	return answer;
 }
