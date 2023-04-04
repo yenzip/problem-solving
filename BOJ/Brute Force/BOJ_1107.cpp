56a8158c8508f7d227cdc16457322c941c99e544
EomYerim <djadpfa1002@naver.com>
2023-04-04 16:33:00 +0900

    리모컨

--- BOJ/Brute Force/BOJ_1107.cpp
+++ BOJ/Brute Force/BOJ_1107.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N, M;
 bool buttons[10];
 
 int check(string s) {
@@ -20,9 +21,7 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int N, M;
 	cin >> N >> M;
-
 	memset(buttons, true, sizeof(buttons));
 	for (int i = 0; i < M; i++) {
 		int block;
@@ -30,13 +29,13 @@ int main() {
 		buttons[block] = false;
 	}
 
-	int answer = abs(N - 100);
+	int answer = abs(N - 100);	// 초기값 : 100에서 N으로 [+/-] 이용해 이동하는 횟수
+
 	for (int channel = 0; channel <= 1000000; channel++) {
-		string tmp = to_string(channel);
-		int count = check(tmp);
-		if (count != -1) {
-			int move_one = abs(N - channel);	// +/-
-			answer = min(answer, count + move_one);
+		int count = check(to_string(channel));	// channel이 고장난 버튼을 포함하는지 검사
+		if (count != -1) {	// 해당 channel 이용 가능
+			int move_one = abs(N - channel);	// channel에서 N으로 [+/-] 이용해 이동하는 횟수
+			answer = min(answer, count + move_one);	// 최소 이동 횟수 갱신
 		}
 	}
 
