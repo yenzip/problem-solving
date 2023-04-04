b6ec0dab281347a02472d123c1032392aad00f4a
EomYerim <djadpfa1002@naver.com>
2023-04-04 18:29:18 +0900

    N과 M (9)

---
+++ BOJ/DFS & BFS/BOJ_15663.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[8];
+bool visited[8];
+int npr[8 + 1];
+set<vector<int>> s;
+
+void dfs(int depth) {
+	if (depth == M) {
+		vector<int> tmp;
+		for (int i = 1; i <= M; i++) {
+			tmp.push_back(npr[i]);
+		}
+		s.insert(tmp);
+		tmp.clear();
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			npr[depth + 1] = arr[i];
+			dfs(depth + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	dfs(0);
+
+	for (auto v : s) {
+		for (int i = 0; i < M; i++) {
+			cout << v[i] << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
