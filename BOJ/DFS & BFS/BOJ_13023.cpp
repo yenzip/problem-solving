927c40fbfbcfd1d918aa065c13ab86171f9e0842
EomYerim <djadpfa1002@naver.com>
2023-04-08 12:19:20 +0900

    ABCDE

--- BOJ/DFS & BFS/BOJ_13023.cpp
+++ BOJ/DFS & BFS/BOJ_13023.cpp
@@ -1,50 +1,46 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int N, M;
-vector<vector<int>> adj;
+vector<vector<int>> graph;
 bool visited[2000];
 int answer = 0;
 
-void dfs(int cur, int depth) {
+void dfs(int now, int depth) {
 	if (depth == 4) {
 		answer = 1;
 		return;
 	}
 
-	for (int next : adj[cur]) {
+	visited[now] = true;
+	for (auto next : graph[now]) {
 		if (!visited[next]) {
-			visited[next] = true;
 			dfs(next, depth + 1);
-			visited[next] = false;
 		}
 	}
+	visited[now] = true;
 }
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> N >> M;
-
-	adj.resize(N);
-
+	graph.resize(N);
 	for (int i = 0; i < M; i++) {
 		int a, b;
 		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
+		graph[a].push_back(b);
+		graph[b].push_back(a);
 	}
 
 	for (int i = 0; i < N; i++) {
-		visited[i] = true;
 		dfs(i, 0);
-		visited[i] = false;
-
-		if (answer == 1) {
+		if (answer) {
 			break;
 		}
 	}
-
-	cout << answer;
+	cout << answer << '\n';
 
 	return 0;
 }
