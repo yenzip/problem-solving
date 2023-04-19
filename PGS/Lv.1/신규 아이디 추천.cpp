f21008c7cb36b2b44effb8aee76c3b0a058b5892
EomYerim <djadpfa1002@naver.com>
2023-04-19 13:19:59 +0900

    Lv.1 문제 풀이 2

--- PGS/Lv.1/신규 아이디 추천.cpp
+++ PGS/Lv.1/신규 아이디 추천.cpp
@@ -1,54 +1,49 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <regex>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 string solution(string new_id) {
+	string answer = "";
 
-	// 1단계 + 2단계
-	for (auto& i : new_id) {
-		i = tolower(i);
-		if (!(islower(i) || isdigit(i) || i == '-' || i == '_' || i == '.')) { // 사용 가능한 문자가 아니면 공백으로 바꾸기
-			i = ' ';
+	for (auto c : new_id) {
+		if (isalpha(c)) {    // 1단계
+			answer += tolower(c);
+		}
+		else if (isdigit(c) || c == '-' || c == '_' || c == '.') {   // 2단계
+			if (c == '.' && answer.back() == '.') { // 3단계
+				continue;
+			}
+			answer += c;
 		}
 	}
 
-	// 2단계 - 공백(사용 불가능한 문자) 제거
-	new_id.erase(remove(new_id.begin(), new_id.end(), ' '), new_id.end());
-
-	// 3단계
-	new_id = regex_replace(new_id, regex("[.]+"), ".");
-
 	// 4단계
-	if (new_id[0] == '.') {
-		new_id.erase(0, 1);
+	if (answer.front() == '.') {
+		answer.erase(answer.begin());
 	}
-	if (!new_id.empty() && new_id[new_id.size() - 1] == '.') {
-		new_id.pop_back();
+	if (answer.back() == '.') {
+		answer.pop_back();
 	}
 
 	// 5단계
-	if (new_id.empty()) {
-		new_id += 'a';
+	if (answer.empty()) {
+		answer += 'a';
 	}
 
 	// 6단계
-	if (new_id.size() > 15) {
-		new_id = new_id.substr(0, 15);
-		if (new_id[new_id.size() - 1] == '.') {
-			new_id.pop_back();
+	if (answer.size() >= 16) {
+		answer = answer.substr(0, 15);
+		if (answer.back() == '.') {
+			answer.pop_back();
 		}
 	}
 
 	// 7단계
-	if (new_id.size() <= 2) {
-		char c = new_id.back();
-		while (new_id.size() != 3) {
-			new_id += c;
+	if (answer.size() <= 2) {
+		char c = answer.back();
+		while (answer.size() < 3) {
+			answer += c;
 		}
 	}
-	
-	return new_id;
+
+	return answer;
 }
