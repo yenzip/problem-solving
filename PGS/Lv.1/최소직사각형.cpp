571844c4b95ff8dc04fe6a0d3b40d7275c4ca8cb
EomYerim <djadpfa1002@naver.com>
2023-04-25 11:24:39 +0900

    Lv.1 문제 풀이 2

--- PGS/Lv.1/최소직사각형.cpp
+++ PGS/Lv.1/최소직사각형.cpp
@@ -7,17 +7,14 @@ using namespace std;
 int solution(vector<vector<int>> sizes) {
 	int answer = 0;
 
-	vector<int> w, h;
-	for (int i = 0; i < sizes.size(); i++) {
-		sort(sizes[i].rbegin(), sizes[i].rend());
-		w.push_back(sizes[i][0]);
-		h.push_back(sizes[i][1]);
+	int w = 0, h = 0;
+	for (auto size : sizes) {
+		sort(size.begin(), size.end());
+		w = max(w, size[0]);
+		h = max(h, size[1]);
 	}
 
-	int w_max = *max_element(w.begin(), w.end());
-	int h_max = *max_element(h.begin(), h.end());
-
-	answer = w_max * h_max;
+	answer = w * h;
 
 	return answer;
 }
