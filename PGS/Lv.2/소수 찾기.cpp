fdf2a895dc66b90ba1b1dd27e2dd2568785dcaa3
EomYerim <djadpfa1002@naver.com>
2023-06-17 17:24:43 +0900

    Lv.2 문제 풀이
    :Explanation:
    소수 찾기
    
    :Language:
    C++
    
    :Algorithm:
    완전 탐색
    
    :Link:
    https://school.programmers.co.kr/learn/courses/30/lessons/42839

--- PGS/Lv.2/소수 찾기.cpp
+++ PGS/Lv.2/소수 찾기.cpp
@@ -1,49 +1,40 @@
-#include <string>
-#include <vector>
-#include <set>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-set<int> s;
 bool visited[7];
+set<int> s;
 
-void dfs(string numbers, string snum) {
-	if (!snum.empty()) {
-		s.insert(stoi(snum));
+bool isPrime(int x) {
+	if (x < 2) {
+		return false;
+	}
+
+	for (int i = 2; i * i <= x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void makeNumber(string num, string numbers) {
+	if (!num.empty() && isPrime(stoi(num))) {
+		s.insert(stoi(num));
 	}
 
 	for (int i = 0; i < numbers.size(); i++) {
 		if (!visited[i]) {
 			visited[i] = true;
-			dfs(numbers, snum + numbers[i]);
+			makeNumber(num + numbers[i], numbers);
 			visited[i] = false;
 		}
 	}
-	return;
-}
-
-bool isPrime(int n) {
-	if (n < 2) {
-		return false;
-	}
-	for (int i = 2; i*i <= n; i++) {
-		if (n %i == 0) {
-			return false;
-		}
-	}
-	return true;
 }
 
 int solution(string numbers) {
-	int answer = 0;
-	
-	dfs(numbers, "");
 
-	for (auto i : s) {
-		if (isPrime(i)) {
-			answer++;
-		}
-	}
+	makeNumber("", numbers);
 
-	return answer;
+	return s.size();
 }
