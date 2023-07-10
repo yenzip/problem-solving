708da6ee070d071df211922fb8846f46ada9a1b0
EomYerim <djadpfa1002@naver.com>
2023-07-10 14:39:03 +0900

    수들의 합 2
    :Explanation:
    부분 수열의 합이 M이 되는 경우의 수 구하기
    
    :Language:
    C++
    
    :Algorithm:
    투 포인터
    
    :Link:
    https://www.acmicpc.net/problem/2003

--- BOJ/2003.cpp
+++ BOJ/2003.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int N;
 long long M;
-long long S[10001];
+int A[10000];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -11,19 +11,24 @@ int main() {
 
 	cin >> N >> M;
 
-	for (int i = 1; i <= N; i++) {
-		int num;
-		cin >> num;
-		S[i] = S[i - 1] + num;
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
 	}
-	
-	int answer = 0;
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= i; j++) {
-			if (S[i] - S[j - 1] == M) {
+	int answer = 0;
+	int start = 0;
+	int end = 0;
+	long long sum = 0;
+
+	while (end <= N) {
+		if (sum <= M) {
+			if (sum == M) {
 				answer++;
 			}
+			sum += A[end++];
+		}
+		else {
+			sum -= A[start++];
 		}
 	}
 
