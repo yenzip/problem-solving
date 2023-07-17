f2ebbe5171a8653f380e6d2148d4465c3ca6aa82
EomYerim <djadpfa1002@naver.com>
2023-07-18 00:55:15 +0900

    뱀
    :Explanation:
    게임 실행 시간 구하기 (재귀✅)
    
    :Language:
    C++
    
    :Algorithm:
    구현 - 시뮬레이션
    큐
    
    :Link:
    https://www.acmicpc.net/problem/3190

--- BOJ/3190.cpp
+++ BOJ/3190.cpp
@@ -2,30 +2,52 @@
 using namespace std;
 
 int N, K, L;
-int board[101][101];
+int A[101][101];
 map<int, char> m;	// { 초, 방향 }
 bool visited[101][101];
 int dx[4] = { -1, 0, 1, 0 };	// 북 - 동 - 남 - 서 (시계방향)
 int dy[4] = { 0, 1, 0, -1 };
-int direction;
+queue<pair<int, int>> body;	// 뱀의 몸 좌표
 
 bool isOut(int x, int y) {
 	return (x < 1 || x > N || y < 1 || y > N);
 }
 
-void turnDirection(char c) {
-	if (c == 'L') {	// 왼쪽으로 90도 회전
-		direction -= 1;
-		if (direction == -1) {
-			direction = 3;
-		}
+int turnDirection(char C, int direction) {
+	if (C == 'L') {	// 왼쪽 90도 회전
+		return (direction + 3) % 4;
 	}
-	else {	// 오른쪽으로 90도 회전
-		direction += 1;
-		if (direction == 4) {
-			direction = 0;
-		}
+	else {			// 오른쪽 90도 회전
+		return (direction + 1) % 4;
 	}
+}
+
+void simulation(int x, int y, int direction, int answer) {
+
+	int nx = x + dx[direction];
+	int ny = y + dy[direction];
+
+	if (isOut(nx, ny) || visited[nx][ny]) {	// 벽이나 자신의 몸과 부딫히면 게임 종료
+		cout << answer;
+		return;
+	}
+
+	visited[nx][ny] = true;
+	body.push({ nx, ny });
+
+	if (A[nx][ny] == 1) {	// 이동한 칸에 사과가 있다면
+		A[nx][ny] = 0;
+	}
+	else {	// 이동한 칸에 사과가 없다면, 꼬리칸 비우기
+		visited[body.front().first][body.front().second] = false;
+		body.pop();
+	}
+
+	if (m.count(answer) != 0) {	// 방향 변환
+		direction = turnDirection(m[answer], direction);
+	}
+
+	simulation(nx, ny, direction, answer + 1);
 }
 
 int main() {
@@ -35,9 +57,9 @@ int main() {
 	cin >> N >> K;
 
 	for (int i = 0; i < K; i++) {
-		int x, y;
-		cin >> x >> y;
-		board[x][y] = 1;	// 사과의 위치 1로 표시
+		int a, b;
+		cin >> a >> b;
+		A[a][b] = 1;
 	}
 
 	cin >> L;
@@ -46,48 +68,12 @@ int main() {
 		int a;
 		char b;
 		cin >> a >> b;
-		m[a] = b;	// 뱀의 방향 변환 정보
+		m[a] = b;
 	}
+	
+	body.push({ 1, 1 });
 
-	int answer = 0;
-	int x = 1;
-	int y = 1;
-
-	queue<pair<int, int>> body;	// 뱀의 몸 좌표
-	body.push({ x, y });
-
-	direction = 1;	// 뱀은 처음에 오른쪽(동)을 향한다
-
-	while (true) {
-		answer++;
-
-		int nx = x + dx[direction];
-		int ny = y + dy[direction];
-
-		if (isOut(nx, ny) || visited[nx][ny]) {	// 벽 또는 몸과 부딫히면 게임 오버
-			break;
-		}
-
-		visited[nx][ny] = true;
-		body.push({ nx, ny });
-
-		if (board[nx][ny] == 1) {	// 이동한 칸에 사과가 있다면
-			board[nx][ny] = 0;	// 사과 없어짐
-		}
-		else {	// 이동한 칸에 사과가 없다면
-			visited[body.front().first][body.front().second] = false;	// 꼬리가 위치한 칸 비워준다
-			body.pop();
-		}
-
-		x = nx;
-		y = ny;
-
-		if (m.count(answer) != 0) {	// 방향 전환
-			turnDirection(m[answer]);
-		}
-	}
-
-	cout << answer;
+	simulation(1, 1, 1, 1);
 
 	return 0;
 }
