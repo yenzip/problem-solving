eb9eae4ea75c8f9814301ec6c120c1efdccf1872
EomYerim <djadpfa1002@naver.com>
2023-07-17 23:49:52 +0900

    로봇 청소기
    :Explanation:
    청소하는 칸의 개수 구하기 (재귀❌)
    
    :Language:
    C++
    
    :Algorithm:
    구현 - 시뮬레이션
    
    :Link:
    https://www.acmicpc.net/problem/14503

--- BOJ/14503.cpp
+++ BOJ/14503.cpp
@@ -7,39 +7,7 @@ int A[50][50];
 bool visited[50][50];
 int dx[4] = { -1, 0, 1, 0 };	// 북 - 동 - 남 - 서 (시계방향)
 int dy[4] = { 0, 1, 0, -1 };
-int answer;
 
-void simulation(int x, int y, int direction) {
-	if (!visited[x][y] && A[x][y] == 0) {	// 1. 현재 칸 청소
-		visited[x][y] = true;
-		answer++;
-	}
-
-	int curDirection = direction;
-
-	for (int i = 0; i < 4; i++) {
-		int nextDirection = (curDirection + 3) % 4;
-		int nx = x + dx[nextDirection];
-		int ny = y + dy[nextDirection];
-
-		if (!visited[nx][ny] && A[nx][ny] == 0) {	// 3. 주변 4칸 중 청소되지 않은 빈 칸 존재
-			simulation(nx, ny, nextDirection);
-			return;
-		}
-
-		curDirection = nextDirection;
-	}
-
-	int nx = x - dx[curDirection];	// 2. 주변 4칸 중 청소되지 않은 빈칸이 없는 경우
-	int ny = y - dy[curDirection];	// 바라보는 방향을 유지한 채로 후진
-
-	if (A[nx][ny] == 1) {	//	후진할 수 없다면 작동 종료
-		return;
-	}
-	else {
-		simulation(nx, ny, curDirection);
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -53,7 +21,51 @@ int main() {
 		}
 	}
 
-	simulation(x, y, d);
+	int answer = 0;
+	int turnCnt = 0;
+	int direction = d;
+
+	while (true) {
+		if (!visited[x][y] && A[x][y] == 0) {
+			visited[x][y] = true;
+			answer++;
+		}
+
+		int curDirection = direction;
+
+		for (int i = 0; i < 4; i++) {
+			int nextDirection = (curDirection + 3) % 4;
+			int nx = x + dx[nextDirection];
+			int ny = y + dy[nextDirection];
+
+			if (!visited[nx][ny] && A[nx][ny] == 0) {
+				x = nx;
+				y = ny;
+				direction = nextDirection;
+				turnCnt = 0;
+				break;
+			}
+			else {
+				turnCnt++;
+			}
+
+			curDirection = nextDirection;
+		}
+
+		if (turnCnt == 4) {
+			int nx = x - dx[direction];
+			int ny = y - dy[direction];
+
+			if (A[nx][ny] == 1) {
+				break;
+			}
+			else {
+				x = nx;
+				y = ny;
+				turnCnt = 0;
+			}
+		}
+	}
 
 	cout << answer;
 
