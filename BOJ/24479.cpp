242028397d40d7ba663073fb19ef179b53065b1b
EomYerim <djadpfa1002@naver.com>
2023-07-24 20:01:57 +0900

    알고리즘 수업 - 깊이 우선 탐색 1
    :Explanation:
    정점 방문 순서 출력하기
    
    :Language:
    C++
    
    :Algorithm:
    그래프 탐색 - 깊이 우선 탐색
    
    :Link:
    https://www.acmicpc.net/problem/24479

---
+++ BOJ/24479.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, R, u, v;
+vector<vector<int>> graph;
+bool visited[100001];
+int answer[100001];
+int seq;
+
+void dfs(int x) {
+	visited[x] = true;
+	answer[x] = seq++;
+
+	for (int next : graph[x]) {
+		if (!visited[next]) {
+			dfs(next);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M >> R;
+
+	graph.resize(N + 1);
+
+	for (int i = 0; i < M; i++) {
+		cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++) {
+		sort(graph[i].begin(), graph[i].end());
+	}
+
+	seq = 1;
+	dfs(R);
+
+	for (int i = 1; i <= N; i++) {
+		cout << answer[i] << "\n";
+	}
+
+	return 0;
+}
