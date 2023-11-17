aadee0335c0678a5a1bda77722318d6514df3db0
EomYerim <djadpfa1002@naver.com>
2023-11-17 12:36:48 +0900

    Most Common Word
    :Explanation:
    금지된 단어 제외하고 가장 흔하게 등장하는 단어 찾기
    
    :Language:
    JAVA
    
    :Algorithm:
    문자열
    해시를 사용한 집합과 맵
    
    :Link:
    https://leetcode.com/problems/most-common-word/

--- LeetCode/JAVA/819.java
+++ LeetCode/JAVA/819.java
@@ -1,20 +1,22 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         String[] words = paragraph.split("[!?',;.\s]+");
-        Map<String, Boolean> bm = new HashMap<>();
+        Set<String> s = new HashSet<>();
         Map<String, Integer> m = new HashMap<>();
         int maxFreq = 0;
         String answer = "";
         
         for(String ban : banned) {
-            bm.put(ban, true);
+            s.add(ban);
         }
         
         for(String word : words) {
-            if(bm.containsKey(word)) {
+            String lower = word.toLowerCase();
+
+            if(s.contains(lower)) {
                 continue;
             }
-            String lower = word.toLowerCase();
+            
             m.put(lower, m.getOrDefault(lower, 0) + 1);
             maxFreq = Math.max(maxFreq, m.get(lower));
         }
