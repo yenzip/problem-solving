75331e46c5e317b9c28f9a2d932ca4719d8ed44a
EomYerim <djadpfa1002@naver.com>
2023-11-19 14:37:07 +0900

    Group Anagrams
    :Explanation:
    문자열을 애너그램 단위로 그룹핑하기
    
    :Language:
    JAVA
    
    :Algorithm:
    문자열
    해시를 사용한 맵
    정렬
    
    :Link:
    https://leetcode.com/problems/group-anagrams/

--- LeetCode/JAVA/49.java
+++ LeetCode/JAVA/49.java
@@ -14,13 +14,9 @@ class Solution {
         }
         
         for(String key : m.keySet()) {
-            List<String> words = m.get(key);
-            Collections.sort(words); // value(List) 알파벳 정렬
-            answer.add(words);
+            answer.add(m.get(key));
         }
         
-        answer.sort((a1, a2) -> (a1.size() < a2.size()) ? -1 : (a1.size() == a2.size()) ? 0 : 1);   // List의 크기별 정렬
-
         return answer;
     }
 }
